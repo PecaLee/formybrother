ed36621b88fad1cc5c64419191574ec70cdd71b2
Peca Lee <studiopeca.lee@gmail.com>
2021-07-17 15:41:56 +0900

    설명텍스트가 없을때

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from os import replace
+from time import sleep
 import urllib.request
 
 import requests
@@ -36,7 +37,7 @@ def get_works_data():
     anishkapoorWorks = []
     # count값과 데이터리스트를 만들어 준비
 
-    for url in works_list[0:49]:
+    for url in works_list:
         # 만든 워크리스트를 배열 0번부터 49번까지만 돌린다.
         work = requests.get(url)
         soup = BeautifulSoup(work.text, "html.parser")
@@ -51,8 +52,17 @@ def get_works_data():
         if material_size:
             material_size = material_size.find("p").text.split("\n")
         else:
-            material_size = soup.find(attrs={"class": "block text_block"}).find(
-                attrs={"class": "content"}).text.split("\n")
+            material_size = soup.find(attrs={"class": "block text_block"})
+            if material_size:
+                material_size = material_size.find(
+                    attrs={"class": "content"}).text.split("\n")
+            else:
+                material_size = ["None", "None"]
+        # material_size의 유무에 따라서 조건부 설정
+        # 1. material_size가 class="block small_text"일때 p의 텍스트를 뽑아내서 나누기
+        # 2. material_size가 없을때 class="block text_block"를 가진녀석을 material_size로 설정
+            # 1. material_size가 있을때 class="content"를 가진녀석의 텍스트를 뽑아내서 나누기
+            # 3. material_size가 없을때 material_size를 ["None", "None"]로 할당
 
         title = title_div.find("h1").text
         year = title_div.find("p").text
@@ -79,8 +89,11 @@ def get_works_data():
         anishkapoorWorks.append(workDic)
         # 만든 딕셔너리 타입의 데이터를 리스트에 저장
 
+        print(count)
         count = count + 1
         # 카운팅 숫자 올리기
+        sleep(1)
+        # 차단보험 하나 크롤링 할때마다 1초의 텀을 줌
 
     return anishkapoorWorks
     # 정보 리턴
